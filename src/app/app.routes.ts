d2131781812651c3d1339302ffd260e53b5b1fa6
jorge1467 <91035401+jorge1467@users.noreply.github.com>
2025-03-12 18:14:58 +0100

    Pantalla cursos profesor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
-import { RouterModule } from '@angular/router';  
+import { ProfesorComponent } from './profesor/profesor.component'; // Importa el componente de profesor
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },  // Ruta para la página de inicio
+  { path: 'profesor', component: ProfesorComponent } // Nueva ruta para la pantalla del profesor
 ];
 
-export const AppRoutingModule = RouterModule.forRoot(routes);  
+export const AppRoutingModule = RouterModule.forRoot(routes);
